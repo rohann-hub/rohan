c5648218d798d033f0b50f1f5e84f3065684a69d
rohann <roh4nn001@gmail.com>
2024-08-10 14:59:40 +0530

    clgAssignment.py

--- clg.py
+++ clg.py
@@ -1,4 +1,4 @@
-# 1.	//write a program to accept a number from user and determine if it is a int,float,string . describe data type
+# 1. write a program to accept a number from user and determine if it is a int,float,string . describe data type
 
 user_input = input("Enter a value: ")
 
@@ -21,8 +21,11 @@ else:
 	    number=int(input("enter a value of number: "))
 	    arr.append(number)
 	    print(arr)
+	    avg=sum(arr)/len(arr)
 	print("max value of number is :", max(arr))
 	print("min value of number is: ", min(arr))
+        print("avg value of number is: ", avg)
+
 
 	# 3. write a program in python  to convert to  string into a list.
 	
@@ -33,5 +36,16 @@ else:
 	print(l)
 
 
+# 4. function take argument and returns its data type
+
+	def get_datatype(value):
+	    return type(value).__name__
+	
+	print(get_datatype(10))         
+	print(get_datatype(3.14))       
+	print(get_datatype("Hello"))   
+	print(get_datatype([1, 2, 3]))  
+	print(get_datatype(None))
+	
 
 	
