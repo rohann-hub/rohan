5a6e889647524557b51df72944430c8e0eff7ad0
rohann <roh4nn001@gmail.com>
2024-12-08 21:15:58 +0530

    Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -39,7 +39,33 @@ def merge(left , right):
 array = [10,30,50,40,60,80,101]
 print("after merge list: " , merge_sort(array))
 
+## Quick sort:
+def quick_sort(arr , low ,high):
+    if low < high:
+        pivot = partition(arr , low , high)
+        quick_sort(arr , low ,  pivot - 1)
+        quick_sort(arr , pivot + 1 , high)
+    return arr
 
+def partition(arr , low , high):
+    p = arr[low]
+    i = low + 1
+    j = high
+    while True:
+        while i <= j and arr[i] <= p:
+            i +=1
+        while i <= j and arr[j] >= p:
+            j -=1
+        if i < j:
+            arr[i],arr[j] = arr[j],arr[i]
+        else:
+            break
+    arr[low],arr[j] = arr[j],arr[low]
+    return j
+
+array = [ 5,8,1,2,6,3,9]
+sorted_array = quick_sort(array , 0 , len(array) - 1)
+print("sorted elements are: " , sorted_array)
 
 
 
