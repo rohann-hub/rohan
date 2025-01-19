314ee7e9616edcc1b796acd800c505728531f4e9
rohann <roh4nn001@gmail.com>
2025-01-19 19:22:20 +0530

    Update newQueue.py

--- newQueue.py
+++ newQueue.py
@@ -1,3 +1,87 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.rear is None:
+            self.front = self.rear = new_node
+            return
+        self.rear.next = new_node
+        self.rear = new_node
+
+    
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        temp = self.front
+        self.front = temp.next
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+    
+    def is_empty(self):
+        return self.front is None
+
+    
+    def display(self):
+        current = self.front
+        while current:
+            print(current.data, end="--->")
+            current = current.next
+        print("none")
+
+
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.display()  
+
+print("remove element: ", q.dequeue())
+print("remove element: ", q.dequeue())
+
+    
+
+q.display() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### OR ##########
 class Queue:
     def __init__(self, size):
         self.queue = []
