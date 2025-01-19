53fa64d3c78dea252b6109ef35c01d10194921e8
rohann <roh4nn001@gmail.com>
2025-01-19 15:27:40 +0530

    Sorting

--- Sorting.py
+++ Sorting.py
@@ -67,7 +67,7 @@ array = [int(x) for x in input("Enter a number :").split()]
 sorted_array = quick_sort(array , 0 , len(array) - 1)
 print("sorted elements are: " , sorted_array)
 
-###    OR   ###
+##### OR ########
 array = [10, 90, 50, 60, 98, 80, 101]
 sorted_array = quick_sort(array, 0, len(array) - 1)
 print("After sorting:", sorted_array)
@@ -75,18 +75,14 @@ print("After sorting:", sorted_array)
 
 ##insertiin sort
 def insertion_sort(arr):
-    # Traverse from 1 to len(arr)
+ 
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        # Move elements of arr[0..i-1], that are greater than key, to one position ahead
-        # of their current position
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
-
-
 arr = [12, 11, 13, 5, 6]
 print("Original array:", arr)
 insertion_sort(arr)
