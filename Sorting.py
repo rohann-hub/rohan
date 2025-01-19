2b013404186d2b68651f61ea0ee0cf30d38e561e
rohann <roh4nn001@gmail.com>
2025-01-19 22:33:39 +0530

    Merge , selection , insertion , Bubble , Quick

--- Sorting.py
+++ Sorting.py
@@ -88,6 +88,20 @@ print("Original array:", arr)
 insertion_sort(arr)
 print("Sorted array:", arr)
 
+## SELECTON SORT
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+       min_index = i
+       for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+array = [64, 25, 12, 22, 11]
+selection_sort(array)
+print("Sorted array:", array)
+
 
 
 
